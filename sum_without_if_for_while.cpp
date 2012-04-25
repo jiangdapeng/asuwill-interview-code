81b2cb3c4e7fdd52a9e35255908165e381c102bb
Dapeng Jiang <asuwill.jdp@gmail.com>
2012-04-25 10:04:34 +0800

    ..

--- sum_without_if_for_while.cpp
+++ sum_without_if_for_while.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 
-
 using namespace std;
 
+/*
+第12题
+题目：求1+2+…+n，
+要求不能使用乘除法、for、while、if、else、switch、case等关键字以及条件判断语句（A?B:C）
+*/
 //test sum_without_if_for
 //#include "sum_without_if_for.h"
+
+
 
 int(*func[2])(int n);
 
