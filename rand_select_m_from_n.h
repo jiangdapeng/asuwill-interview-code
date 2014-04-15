6a7256c023134e102dfe46fde267c90bdff7a13b
DapengJiang <asuwill.jdp@gmail.com>
2014-04-15 16:49:14 +0800

    Update rand_select_m_from_n.h

--- rand_select_m_from_n.h
+++ rand_select_m_from_n.h
@@ -11,6 +11,7 @@ float frand()
 /*
 * 从给定数据集中随机选择m个
 * 保证每一个数被选到的概率一样
+* 原理：设m’为还需要从A中选取的元素个数， n’为元素a_i及其右边的元素个数， 也即n’=(n-i+1)。那么选取元素a_i的概率为 m’/n’
 */
 template<class T>
 bool rand_select(const std::vector<T> vecData, int m, std::vector<T>& vecRand)
